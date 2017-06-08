0dca8516e4b326b7cde0617986fbd15dd190ab5e
Lucie Charvat <luci.charvat@gmail.com>
2017-06-08 22:12:41 +0200

    documentation-card: card showing relevant information about a library function or a data type
    Showing the output and input types of a functions on hover.

--- plugins/devhelp/gbp-devhelp-editor-view-addin.c
+++ plugins/devhelp/gbp-devhelp-editor-view-addin.c
@@ -58,6 +58,108 @@ request_documentation_cb (GbpDevhelpEditorViewAddin *self,
   gbp_devhelp_panel_focus_search (GBP_DEVHELP_PANEL (panel), word);
 }
 
+static gchar *
+remove_tags (gchar *source_html)
+{
+  GString *text = g_string_new (NULL);
+  gboolean in_tag = FALSE;
+  gboolean white_sym = FALSE;
+
+  while (*source_html)
+    {
+      gunichar ch = g_utf8_get_char (source_html);
+      if (ch == '<')
+        in_tag = TRUE;
+      else if (ch == '>')
+        {
+          in_tag = FALSE;
+          if (!white_sym)
+              white_sym = TRUE;
+        }
+      else if (g_unichar_isspace (ch) && !white_sym)
+          white_sym = TRUE;
+      else if (!in_tag && !g_unichar_isspace (ch))
+        {
+          if (white_sym && ch != ',')
+            g_string_append_unichar (text, ' ');
+          white_sym = FALSE;
+          g_string_append_unichar (text, ch);
+        }
+      source_html = g_utf8_next_char (source_html);
+    }
+
+  return g_string_free (text, FALSE);
+}
+
+static gchar *
+xml_parse (gchar *file_name,
+           gchar *func_name)
+{
+  GFileInputStream *file_stream;
+  GDataInputStream *data_stream;
+  GString *source = g_string_new (NULL);
+  GError *error = NULL;
+  GMatchInfo *match_info;
+  GRegex *regex;
+  GRegex *regex_start;
+  GRegex *regex_end;
+  const gchar *regex_char;
+  gchar *line;
+  gchar *line_start;
+  gchar *line_end;
+  gboolean found_tag = FALSE;
+  gboolean start_tag = FALSE;
+
+  file_stream = g_file_read (g_file_new_for_uri (file_name), NULL, &error);
+  if (file_stream == NULL)
+    return g_string_free (source, FALSE);
+
+  regex_char = g_strdup_printf ("name=\"%s\"", func_name);
+  regex = g_regex_new (regex_char, 0, 0, NULL);
+  regex_start = g_regex_new (".*<pre.*?>", 0, 0, NULL);
+  regex_end = g_regex_new ("</pre.*", 0, 0, NULL);
+
+  data_stream = g_data_input_stream_new (G_INPUT_STREAM (file_stream));
+
+  while (TRUE)
+    {
+      line = g_data_input_stream_read_line_utf8 (data_stream, NULL, NULL, &error);
+      if (error != NULL || line == NULL)
+        return g_string_free (source, FALSE);
+
+      if (!found_tag)
+        {
+	        g_regex_match (regex, line, 0, &match_info);
+          if (g_match_info_matches (match_info))
+            found_tag = TRUE;
+        }
+      if (found_tag && !start_tag)
+        {
+          g_regex_match (regex_start, line, 0, &match_info);
+          if (g_match_info_matches (match_info))
+            {
+              start_tag = TRUE;
+              line_start = g_regex_replace (regex_start, line, -1, 0, "", 0, NULL);
+              g_free (line);
+              line = line_start;
+            }
+        }
+      if (found_tag && start_tag)
+        {
+          g_regex_match (regex_end, line, 0, &match_info);
+          if (g_match_info_matches (match_info))
+            {
+              line_end = g_regex_replace (regex_end, line, -1, 0, "", 0, NULL);
+              g_string_append (source, line_end);
+              break;
+            }
+          g_string_append (source, line);
+        }
+    }
+
+  return remove_tags (g_string_free (source, FALSE));
+}
+
 static gboolean
 query_tooltip_cb (GbpDevhelpEditorViewAddin *self,
                   gint                       x,
@@ -73,9 +175,10 @@ query_tooltip_cb (GbpDevhelpEditorViewAddin *self,
   GtkTextIter end;
   DhKeywordModel *keyword_model;
   DhLink *link;
-  gchar *selected_text = NULL;
-  gchar *uri = NULL;
-  gchar *book_name = NULL;
+  const gchar *selected_text;
+  const gchar *uri;
+  // const gchar *book_name = NULL;
+  gchar **tokens;
 
   g_assert (GTK_IS_WIDGET (widget));
   g_assert (GBP_IS_DEVHELP_EDITOR_VIEW_ADDIN (self));
@@ -90,7 +193,7 @@ query_tooltip_cb (GbpDevhelpEditorViewAddin *self,
     return FALSE;
 
   lang = gtk_source_buffer_get_language (GTK_SOURCE_BUFFER (buffer));
-  if (!ide_str_equal0 (gtk_source_language_get_id(lang), "c"))
+  if (lang == NULL || !ide_str_equal0 (gtk_source_language_get_id(lang), "c"))
     return FALSE;
 
   gtk_text_view_window_to_buffer_coords (GTK_TEXT_VIEW (source_view), GTK_TEXT_WINDOW_WIDGET, x, y, &x, &y);
@@ -105,10 +208,10 @@ query_tooltip_cb (GbpDevhelpEditorViewAddin *self,
     gtk_text_iter_forward_char (&end);
 
   selected_text = gtk_text_buffer_get_text (buffer, &begin, &end, FALSE);
-  if (selected_text == NULL)
+  if (selected_text == NULL || g_strcmp0 (selected_text, "") == 0)
     return FALSE;
 
-  if (self->previous_text != selected_text)
+  if (g_strcmp0 (self->previous_text, selected_text) != 0)
     {
       keyword_model = dh_keyword_model_new ();
       link = dh_keyword_model_filter (keyword_model, selected_text, NULL, NULL);
@@ -116,56 +219,34 @@ query_tooltip_cb (GbpDevhelpEditorViewAddin *self,
         return FALSE;
 
       uri = dh_link_get_uri (link);
-      book_name = dh_link_get_book_name (link);
-      self->tooltip_text = g_strdup_printf ("%s >> %s", book_name, selected_text);
+      // book_name = dh_link_get_book_name (link);
+      tokens = g_strsplit (uri, "#", -1 );
+      if (tokens == NULL)
+        {
+          return FALSE;
+        }
+
+      if (g_strv_length (tokens) != 2)
+        {
+          g_strfreev (tokens);
+          return FALSE;
+        }
+
+      g_free (self->tooltip_text);
+      self->tooltip_text = xml_parse (tokens[0], tokens[1]);
+      g_strfreev (tokens);
+      if (g_strcmp0 (self->tooltip_text, "") == 0)
+        return FALSE;
+
+      g_free (self->previous_text);
+      self->previous_text = g_strdup (selected_text);
     }
-  else
-    self->previous_text = selected_text;
 
   gtk_tooltip_set_text (tooltip, self->tooltip_text);
 
   return TRUE;
 
 }
-
-/* static void */
-/* gbp_devhelp_view_addin_action (GSimpleAction *action, */
-/*                                GVariant      *variant, */
-/*                                gpointer       user_data) */
-/* { */
-/*   IdeSourceView *source_view; */
-/*   xmlTextReaderPtr reader; */
-/*   gchar *selected_text = NULL; */
-/*   gchar *file_name = NULL; */
-/*   gchar *function_name = NULL; */
-/*   gint ret; */
-
-/*   g_print ("Book name: %s\n", dh_link_get_book_name (link)); */
-/*   g_print ("File name: %s\n", dh_link_get_file_name (link)); */
-/*   g_print ("Uri: %s\n", dh_link_get_uri (link)); */
-
-
-  /* reader = xmlReaderForFile(file_name, NULL, 0); */
-  /* if (reader != NULL) { */
-  /*   ret = xmlTextReaderRead(reader); */
-  /*   while (ret == 1) */
-  /*     { */
-  /*       if (xmlTextReaderConstName(reader) != NULL) */
-  /*         { */
-  /*           g_print("%d %d %s %d %d\n", */
-	 /*            xmlTextReaderDepth(reader), */
-	 /*            xmlTextReaderNodeType(reader), */
-	 /*            xmlTextReaderConstName(reader), */
-	 /*            xmlTextReaderIsEmptyElement(reader), */
-	 /*            xmlTextReaderHasValue(reader)); */
-  /*         } */
-  /*       ret = xmlTextReaderRead(reader); */
-  /*     } */
-  /*     xmlFreeTextReader(reader); */
-  /*     if (ret != 0) */
-  /*       g_print ("%s : failed to parse\n", file_name); */
-  /* } */
-/* } */
 
 static void
 gbp_devhelp_editor_view_addin_load (IdeEditorViewAddin *addin,
